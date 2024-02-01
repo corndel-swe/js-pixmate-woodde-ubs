50a2ad1acb42c94530b3a715bdd5dee88c2da2a6
nimonian <nimon@hotmail.co.uk>
2024-02-01 23:42:55 +0000

    make it easier

--- tests/2b.test.js
+++ tests/2b.test.js
@@ -7,6 +7,19 @@ describe('stepDown', () => {
     assert.strictEqual(stepDown([3, 4, 2, 5, 6]), 2)
   })
 
+  it('should return -1 for an array where no element is smaller than its predecessor', () => {
+    assert.strictEqual(stepDown([5, 10, 15, 20]), -1)
+    assert.strictEqual(stepDown([1, 2, 3, 4, 5]), -1)
+  })
+
+  it('should return -1 for an empty array', () => {
+    assert.strictEqual(stepDown([]), -1)
+  })
+
+  it('should handle arrays with all identical elements', () => {
+    assert.strictEqual(stepDown([7, 7, 7, 7]), -1)
+  })
+
   it('should handle arrays where the first two elements satisfy the condition', () => {
     assert.strictEqual(stepDown([3, 2, 4, 5, 6]), 1)
   })
