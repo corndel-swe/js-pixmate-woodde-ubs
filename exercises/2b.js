8adf989d5960598fd47d90b35e7733d09b3d3e03
nimonian <nimon@hotmail.co.uk>
2024-02-01 23:39:14 +0000

    a little easier

--- exercises/2b.js
+++ exercises/2b.js
@@ -1,17 +1,13 @@
 /**
  * Finds the index of the first element in an array that is smaller than the element before it.
+ * (This is guaranteed to occur for all input arrays.)
  *
  * @example
  * // returns 3
  * stepDown([10, 12, 15, 14, 16, 20])
  *
- * @example
- * // returns -1 (no element is smaller than its predecessor)
- * stepDown([5, 10, 15, 20])
- *
  * @param {number[]} numbers - An array of numbers to be examined.
  * @returns {number} The index of the first element that is smaller than its predecessor.
- *                   Returns -1 if no such element is found or if the array is empty.
  */
 export function stepDown(numbers) {
   // TODO
